13cc2327093aa5ec1d06eb4ccdcf8c8d5a4990da
mattsel <matt.sel04@gmail.com>
2023-12-14 09:49:06 -0500

    Created the ability to test for similarity between password and email.

---
+++ main.py
@@ -0,0 +1,56 @@
+import Levenshtein
+
+special_character = "!@#$%^&*()-+?_=,<>/."
+strength_meter = [0]
+threshold = 0.3
+while True:
+    email = input("Please enter your email: ")
+    email_index = email.find('@')
+    if email_index == -1:
+        print("Invalid Email Address")
+    else:
+        email = email[:email_index]
+        break
+
+password = input("Please Enter your Password Here: ")
+
+def special_test(password, strength_meter, special_character):
+    if any(c in special_character for c in password):
+        strength_meter[0] += 1
+
+def capital_test(password, strength_meter):
+    if any(char.isupper() for char in password):
+        strength_meter[0] += 1
+
+def length_test(password, strength_meter):
+    if len(password) >= 12:
+        strength_meter[0] += 1
+
+def similarity_test(password, email):
+    distance = Levenshtein.distance(password, email)
+    max_length = max(len(password), len(email))
+    similarity = 1 - (distance / max_length)
+    return similarity
+similarity = similarity_test(password, email)
+
+def meter(strength_meter):
+    if strength_meter[0] == 3:
+        return "Excellent Password"
+    elif strength_meter[0] == 2:
+        return "Password is Good"
+    elif strength_meter[0] == 1:
+        return "Password is Weak"
+
+special_test(password, strength_meter, special_character)
+capital_test(password, strength_meter)
+length_test(password, strength_meter)
+
+def output(similarity, threshold, strength_meter):
+    if similarity > threshold:
+        return "The password provided and your email are too similar."
+    elif strength_meter[0] > 0:
+        return meter(strength_meter)
+    else:
+        return "Please make sure your password includes a special character, capital letter, and is at least 12 characters long."
+output_result = output(similarity, threshold, strength_meter)
+print(output_result)
