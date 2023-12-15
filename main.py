1eb9a8eb12abf68cfd8c0f6b64ef500064552b8c
mattsel <matt.sel04@gmail.com>
2023-12-14 23:32:33 -0500

    I included a new function that can know store the users email and password as a sha256 hash

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import Levenshtein
-#Initiallizes variables
+import hashlib
+
+client_info = "login-info.txt"
 special_character = "!@#$%^&*()-+?_=,<>/."
 strength_meter = [0]
 threshold = 0.3
 ciphertext = ""
-#Function that converts user input to ceaser cypher encryption. User can select a value to shift the letters by.
+
 def ceaser_encryption(password, n):
     global ciphertext 
     ciphertext = ""
@@ -17,45 +19,42 @@ def ceaser_encryption(password, n):
         else:
             ciphertext += char
     return ciphertext
-#Function that is able runs the ceaser encryption, but with a negative shift to decrypt the message. 
+
 def ceaser_decryption(ciphertext, n):
     return ceaser_encryption(ciphertext, -n)
-#Tests if the function includes a special character as defined in the initial variables. 
+
 def special_test(password, strength_meter, special_character):
     if any(c in special_character for c in password):
         strength_meter[0] += 1
-#Tests to see if the password includes a capital letter.
+
 def capital_test(password, strength_meter):
     if any(char.isupper() for char in password):
         strength_meter[0] += 1
-#Tests to see if the function is at least 12 characters long to ensure security of the password. 
+
 def length_test(password, strength_meter):
     if len(password) >= 12:
         strength_meter[0] += 1
-#Checks to see if the user has a numerical value included inside of their password. 
+
 def numerical_test(password, strength_meter):
     if any(c.isdigit() for c in password):
         strength_meter[0] += 1
-#Tests for user's password and email to determine if they are similar to one another. 
+
 def similarity_test(password, email):
     distance = Levenshtein.distance(password, email)
     max_length = max(len(password), len(email))
     similarity = 1 - (distance / max_length)
     return similarity
-#Each time a function has passed a test, the strength_meter is incremented by one which will determine the passwords strength. 
+
 def meter(strength_meter):
     if strength_meter[0] == 4:
-        return "\nExcellent Password"
+        return "\nYour password is considered EXCELLENT and unique"
     elif strength_meter[0] == 3:
-        return "\nPassword is Strong"
+        return "\nYour Password is considered strong"
     elif strength_meter[0] == 2:
-        return "\nPassword is Weak"
-    elif strength_meter[0] == 1:
-        return "\nVery Weak Password"
-#The output will test to see if the calculation of similarity is passed the threshold defined in the variable initialization. 
-#If the condition is true, the program will inform the user about the similarity issue to avoid having a vulnerable password.
-#The other condition will run the strenght meter and depending on the amount of tests that it has passed, it will output the password's strength.
-#If the user hasn't passed any of the tests, it will remind the user of some of the recommended requirements for creating a valid password. 
+        return "\nYour password is weak. Please consider making it more complex and diversified in order to make for a safer password."
+    elif strength_meter[0] == 1: 
+        return "\nVery Weak Password. This password is very simple and can be easily guessed."
+
 def output(similarity, threshold, strength_meter):
     if similarity > threshold:
         return "\nThe password provided and your email are too similar. This can lead to a potentially vulnerable password.\n"
@@ -63,22 +62,26 @@ def output(similarity, threshold, strength_meter):
         return meter(strength_meter)
     else:
         return "\nPlease make sure your password includes at least one of the following: a special character, capital letter, 12 characters long, or a numerical value.\n"
-#This function will only accept an email if it includes an @ symbol. Without it will output a invalid email response.
-#After the user enters a valid email, the system will ONLY test the similarity of the email, before the @ symbol because of the slice in the string. 
+    
+def data_storing(email_initial, password):
+    email_hash = hashlib.sha256(email_initial.encode()).hexdigest()
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+    return email_hash, password_hash
+
 while True:
-    email = input("\nPlease enter your email: ")
-    email_index = email.find('@')
+    email_initial = input("\nPlease enter your email: ")
+    email_index = email_initial.find('@')
     if email_index == -1:
         print("\nInvalid Email Address")
     else:
-        email = email[:email_index]
+        email = email_initial[:email_index]
         break
-#Prompts that the user will be initially asked. Depending on their response will determine the programs next steps. 
+    
 password = input("\nPlease Enter your Password Here: ")
+data_email, data_password = data_storing(email_initial, password)
 while True:
     ceaser_encryption_ques = input("\nWould you like to utilize an encrypted version of your password? If yes, reply 'y'. If not, reply 'n': ")
     ceaser_encryption_flag = ceaser_encryption_ques.lower()
-    #If the user selects to encrypt their password, they will be prompted to select a shift value. This will then print the users encrypted and decrypted version of their password to ensure accuracy. 
     if ceaser_encryption_flag == "y":
         n = int(input("\nSelect a random value between (0 --> 25): "))
         password = ceaser_encryption(password, n)
@@ -87,7 +90,7 @@ while True:
         print("Thank you for using our Ceaser Cypher Password Encryption.\nYour password is now considered fairly safe to use considering the use of randomness of ciphertext as a password.")
         print("Although you know have a encrypted password, please avoid sharing your password with others because it can easily cause for your password to be compromised.\n")
         break
-    #If the user decides to not use the encryption system, the program will continue to test it's strength by running it through the other series of tests.
+   
     elif ceaser_encryption_flag == "n":
         numerical_test(password, strength_meter)
         special_test(password, strength_meter, special_character)
@@ -95,6 +98,9 @@ while True:
         length_test(password, strength_meter)
         similarity = similarity_test(password, email)
         output_result = output(similarity, threshold, strength_meter)
+        with open(client_info, "a") as file:
+            file.write(data_email + ": ")
+            file.write(data_password + "\n")
         print(output_result) 
         break
     else:
